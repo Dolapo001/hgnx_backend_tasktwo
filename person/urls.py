6f1deb14344b58bce992f9460b30582047a982fa
Dolapo24 <adedolapovictoria927@gmail.com>
2023-09-13 15:08:44 +0100

    task 2

--- person/urls.py
+++ person/urls.py
@@ -3,5 +3,5 @@ from .views import CreatePersonView, PersonDetailView
 
 urlpatterns = [
     path('', CreatePersonView.as_view(), name='create-person'),
-    path('<int:pk>/', PersonDetailView.as_view(), name='person-detail'),
+    path('<int:pk>', PersonDetailView.as_view(), name='person-detail'),
 ]
