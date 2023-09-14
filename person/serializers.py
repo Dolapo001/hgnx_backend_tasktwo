01b9e72344cc05cbb29437304c08501879f325da
Dolapo24 <adedolapovictoria927@gmail.com>
2023-09-14 11:44:27 +0100

    task 2

--- person/serializers.py
+++ person/serializers.py
@@ -3,6 +3,8 @@ from .models import Person
 
 
 class PersonSerializer(serializers.ModelSerializer):
+    name = serializers.Charfield(required=True)
+
     class Meta:
         model = Person
         fields = ('id', 'name', 'age', 'email')
