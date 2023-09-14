dc5464be4e071d7006aaebe8c089f35674a950e4
Dolapo24 <adedolapovictoria927@gmail.com>
2023-09-14 01:22:33 +0100

    task 2

--- person/urls.py
+++ person/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreatePersonView, PersonDetailView
+from .views import CreatePersonView, PersonDetailView, PersonByNameView
 
 urlpatterns = [
-    path('', CreatePersonView.as_view(), name='create-person'),
-    path('<int:identifier>', PersonDetailView.as_view(), name='person-detail'),
+    path('api/', CreatePersonView.as_view(), name='create-person'),
+    path('api/<str:pk>/', PersonDetailView.as_view(), name='person-detail'),
+    path('api/<str:name__iexact>', PersonByNameView.as_view(), name='person-by-name'),
 ]
--- person/views.py
+++ person/views.py
@@ -1,29 +1,19 @@
-from rest_framework.generics import get_object_or_404
-
-from .models import Person
-from .serializers import PersonSerializer
+from django.http import Http404
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from django.urls import reverse
+from .models import Person
+from .serializers import PersonSerializer
 
 
-class CreatePersonView(generics.ListCreateAPIView):
+class CreatePersonView(generics.CreateAPIView, generics.ListAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    lookup_field = 'identifier'
-
-    def get_object(self):
-        identifier = self.kwargs['identifier']
-
-        if identifier.isdigit():
-            return get_object_or_404(self.queryset, id=identifier)
-        else:
-            return get_object_or_404(self.queryset, name=identifier)
 
     def perform_create(self, serializer):
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers={'Location': reverse('create-person')})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class PersonDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -40,11 +30,29 @@ class PersonDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def perform_destroy(self, instance):
         instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT, headers={'Location': reverse('create-person')})
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+
+class PersonByNameView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    lookup_field = 'name__iexact'
+
+    def get_object(self):
+        lookup_value = self.kwargs[self.lookup_field]
+        try:
+            return self.queryset.get(**{self.lookup_field: lookup_value})
+        except Person.DoesNotExist:
+            raise Http404("Person does not exist")
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
+    def perform_update(self, serializer):
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK, headers={'Location': reverse('person-detail', args=[serializer.instance.pk])})
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
