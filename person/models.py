dc86de78b80c422032f8ca5088de871a7913b03b
Dolapo24 <adedolapovictoria927@gmail.com>
2023-09-14 12:03:29 +0100

    task 2

--- person/models.py
+++ person/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.PositiveIntegerField(blank=True, null=True)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(blank=True)
 
     def __str__(self):
         return self.name
