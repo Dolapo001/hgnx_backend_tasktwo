e68454fba58107d817047de15a4cde2cf1eca7e4
Dolapo24 <adedolapovictoria927@gmail.com>
2023-09-14 01:37:34 +0100

    task 2

--- person/views.py
+++ person/views.py
@@ -64,9 +64,4 @@ class PersonByNameView(generics.RetrieveUpdateDestroyAPIView):
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+
