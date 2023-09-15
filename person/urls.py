37696d524283e178d8add6e00957bdd0153a5583
Dolapo24 <adedolapovictoria927@gmail.com>
2023-09-15 20:44:29 +0100

    task 2

--- person/urls.py
+++ person/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/<int:pk>/', views.PersonDetailView.as_view(), name='person-detail'),
+    path('api/<int:pk>', views.PersonDetailView.as_view(), name='person-detail'),
     path('api/<str:name__iexact>/', views.PersonByNameView.as_view(), name='person-by-name'),
     path('api/', views.CreatePersonView.as_view(), name='create-person'),
 ]
